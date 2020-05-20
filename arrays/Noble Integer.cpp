e76cb46eabeea31ad53e091db311c7ade3d45b44
Swagat Nayak <55399606+swagatn22@users.noreply.github.com>
2020-05-20 23:50:19 +0530

    Create  Noble Integer.cpp

---
+++ arrays/Noble Integer.cpp
@@ -0,0 +1,11 @@
+int Solution::solve(vector<int> &A) {
+ sort(A.begin(),A.end());
+ if(A[A.size() - 1] == 0) return 1;
+ for(int i = 0; i < A.size(); i++){
+     if(A[i] == A[i + 1]) continue;
+     else if(A[i] == A.size()-i-1){
+         return 1;
+     }
+ }
+ return -1;
+}
